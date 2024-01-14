5ebdb3e6fce5dfcd8dff02f457d77b9df2008f6c
Sarp Dengizmen <e.dengizmen@gmail.com>
2024-01-14 16:41:21 +0300

    modified

--- Arduino_relay/sketch_jan7a/sketch_jan7a.ino
+++ Arduino_relay/sketch_jan7a/sketch_jan7a.ino
@@ -1,25 +1,50 @@
-const byte ledPin = 13;
 const byte interruptPin = 2;
-volatile byte state = LOW;
 bool val;
 const byte switchpin = 3;
+volatile int rev = 0;
+unsigned long timer = 0;
+unsigned long time_taken = 0;
+long rpm = 0;
+bool flag = 0;
+
 void setup() {
   pinMode(interruptPin, INPUT);
   pinMode(switchpin, OUTPUT);
   Serial.begin(9600);   
+  attachInterrupt(digitalPinToInterrupt(interruptPin), revcounter, RISING);
+  timer = millis();
 }
 
 void loop() {
-  val = digitalRead(interruptPin);
-  Serial.println(val);
-  if (val == 1) {
-    digitalWrite(switchpin, HIGH);
-  } else {
-    digitalWrite(switchpin, LOW);
+  if (rev >= 5) {
+    // disable interrupts
+    detachInterrupt(digitalPinToInterrupt(interruptPin));
+    time_taken = millis() - timer;
+    rpm = (rev * 60000) / time_taken;
+    Serial.println(rpm);
+    if (rpm >= 400) {
+      digitalWrite(switchpin, HIGH);
+    }
+    else {
+      digitalWrite(switchpin, LOW);
+    }
+    // enable interrupts
+    attachInterrupt(digitalPinToInterrupt(interruptPin), revcounter, RISING);
+    timer = millis();
+    rev = 0;
   }
-  
+  else if (flag == 1){
+    //sleep 50ms
+    delay(50);
+    flag = 0;
+  }
 }
 
-void blink() {
-  state = !state;
+void revcounter() {
+  if (flag == 0){
+    rev = rev + 1;
+    flag = 1;
+  }
+
+
 }
