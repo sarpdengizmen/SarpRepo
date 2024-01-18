a26d78e4c998ae4176520c27170ebba411fb0c53
Sarp Dengizmen <e.dengizmen@gmail.com>
2024-01-18 05:27:39 +0300

    updated

--- Arduino_relay/sketch_jan7a/sketch_jan7a.ino
+++ Arduino_relay/sketch_jan7a/sketch_jan7a.ino
@@ -1,3 +1,9 @@
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
 const byte interruptPin = 2;
 bool val;
 const byte switchpin = 3;
@@ -6,6 +12,9 @@ unsigned long timer = 0;
 unsigned long time_taken = 0;
 long rpm = 0;
 bool flag = 0;
+const int N = 5; // Number of values to keep for rolling average
+unsigned long rev_times[N];
+int index = 0;
 
 void setup() {
   pinMode(interruptPin, INPUT);
@@ -13,14 +22,22 @@ void setup() {
   Serial.begin(9600);   
   attachInterrupt(digitalPinToInterrupt(interruptPin), revcounter, RISING);
   timer = millis();
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-  if (rev >= 5) {
+  
+  if (rev >= 1) {
     // disable interrupts
     detachInterrupt(digitalPinToInterrupt(interruptPin));
-    time_taken = millis() - timer;
-    rpm = (rev * 60000) / time_taken;
+    time_taken = micros() - timer;
+    Serial.println(time_taken);
+    rpm = (60000000) / time_taken;
+    timer = micros();
+    rev_times[index] = rpm;
+    index = (index + 1) % N;
+    rpm = average(rev_times, N);
     Serial.println(rpm);
     if (rpm >= 400) {
       digitalWrite(switchpin, HIGH);
@@ -28,23 +45,32 @@ void loop() {
     else {
       digitalWrite(switchpin, LOW);
     }
+    rev = 0;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("RPM: ");
+    lcd.print(rpm);
     // enable interrupts
     attachInterrupt(digitalPinToInterrupt(interruptPin), revcounter, RISING);
-    timer = millis();
-    rev = 0;
   }
-  else if (flag == 1){
-    //sleep 50ms
-    delay(50);
-    flag = 0;
+}
+
+// Function to calculate average of an array
+unsigned long average(unsigned long* arr, int length) {
+  unsigned long sum = 0;
+  for (int i = 0; i < length; i++) {
+    sum += arr[i];
   }
+  return sum / length;
 }
 
 void revcounter() {
-  if (flag == 0){
-    rev = rev + 1;
-    flag = 1;
+  // Software debouncing
+  static unsigned long last_interrupt_time = 0;
+  unsigned long interrupt_time = micros();
+  if (interrupt_time - last_interrupt_time > 10000) { // Ignore interrupts closer than 1000 microseconds
+    rev++;
   }
-
-
+  last_interrupt_time = interrupt_time;
 }
+
